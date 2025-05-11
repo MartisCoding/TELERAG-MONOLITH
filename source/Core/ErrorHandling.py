01a98c5f3d60e176fcee9d3269bdc8578517ef81
MartisCoding <gavrilaakimov@gmail.com>
2025-05-11 15:37:58 +0300

    Resolved Circular imports

--- source/Core/ErrorHandling.py
+++ source/Core/ErrorHandling.py
@@ -1,15 +1,6 @@
 import asyncio
 from typing import Optional
-from source.Core.Logging import Logger, aprint_err
-
 class CoreException(Exception):
-    logger_obj: Optional[Logger] = None
-    @classmethod
-    def get_logger(cls):
-        if cls.logger_obj is None:
-            return
-        return cls.logger_obj
-
     def __init__(
             self,
             where: str,
@@ -21,18 +12,6 @@ class CoreException(Exception):
         self.msg_string = f"Exception at {where}: {what}. {'Summary ' + summary if summary else ''} \n {full_message if full_message else ''}"
         self.fatal = fatal
         super().__init__(self.msg_string)
-        self.logger = self.get_logger()
-        if self.logger:
-            asyncio.create_task(self.log_exception())
-
-    async def log_exception(self):
-        try:
-            if self.fatal:
-                await self.logger.fatal(self.msg_string)
-            else:
-                await self.logger.exception(self.msg_string)
-        except Exception as e:
-            await aprint_err(f"Something went wrong while logging exception: {e}")
 
 
 
